3d1236ac1cbbbeee2aef2dd5a50e31f376a8d47c
LeVanHang <vanhang1802@gmail.com>
2022-07-07 23:55:39 +0700

    out done

---
+++ index.js
@@ -0,0 +1,26 @@
+// document.getElementById("demo").innerText = "Xin chao C0622I1";
+// document.getElementById("demo").innerHTML = "<h1>Xin chao</h1>";
+//getElementById có nghĩa là: in ra thành phần có Id là... thẻ con la thành phần
+//innerHTML: bên trongHTML
+//Trả về mã HTML bên trong phần tử hiện tại. Đoạn mã HTML này là chuỗi kí tự chứa tất cả
+// phần tử bên trong, bao gồm các nút phần tử và nút văn bản.
+//innerText im ra chử
+//getElementById("input") để xác nhận phần tử cần sử lý
+//+ Dùng phương thức getElementById(“input_1”) để xác định phần tử cần xử lý
+//
+// + Lấy dữ liệu của thuộc tính value bằng cách truy xuất thuộc tính value của phần tử
+//
+// + Lấy dữ liệu của thuộc tính tự định nghĩa gia_tri bằng phương thức getAttribute(“gia_tri)
+//
+// + Lấy dữ liệu trong phần tử khối div bằng cách truy xuất thuộc tính innerHTML. Ngược lại, để gán giá trị bên trong khối div bằng cách gán thuộc tính innerHTML.
+//
+// Thêm thuộc tính cho phần tử
+//
+// Vẫn với đoạn code bên trên, chúng ta sẽ thêm thuộc tính readonly cho input
+//
+// Set Read only+ Dùng sự kiện onclick để chạy hàm js. Về phần sự kiện – event, mình sẽ nói rõ trong bài học sau.
+//
+// + Sử dụng phương thức getElementById() để xác định phần tử.
+//Thuộc tính innerText trong JavaScript có thể được sử dụng để viết các văn bản động trên các trang web html. Ở đây, văn bản sẽ không được hiểu là văn bản html nhưng là văn bản bình thường.
+//
+// Nó được sử dụng chủ yếu trong các trang web để tạo ra các nội dung động như viết thông báo xác nhận, độ mạnh mật khẩu, v.v.
